4cf2dc502c7ccb0ebdc7243668df1eaa03cc8173
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-11 17:11:02 +0700

    Create structure that contains loaded image information

--- wasm/decode_qr.c
+++ wasm/decode_qr.c
@@ -5,17 +5,24 @@
 #include <jpeglib.h>
 #include <png.h>
 
+struct Image {
+    int width;
+    int height;
+    uint8_t *buffer;
+};
+
 /*
  * Load png data using filename
  * */
-void load_png(struct quirc *q, const char *filename) {
-    int width, height, rowbytes, interlace_type, number_passes = 1;
+struct Image load_png(struct quirc *q, const char *filename) {
+    struct Image img;
+
+    int width, height, interlace_type, number_passes = 1;
     png_uint_32 trns;
     png_byte color_type, bit_depth;
     png_structp png_ptr = NULL;
     png_infop info_ptr = NULL;
     FILE *infile = NULL;
-    uint8_t *image;
     int pass;
 
     infile = fopen(filename, "rb");
@@ -64,7 +71,6 @@ void load_png(struct quirc *q, const char *filename) {
 
     width = png_get_image_width(png_ptr, info_ptr);
     height = png_get_image_height(png_ptr, info_ptr);
-    rowbytes = png_get_rowbytes(png_ptr, info_ptr);
 
     /*
      * Having obtained a decoder object,
@@ -73,11 +79,8 @@ void load_png(struct quirc *q, const char *filename) {
      */
     quirc_resize(q, width, height);
 
-    /*
-     * Using ``quirc_begin`` and ``quirc_end``,
-     * you can feed a grayscale image directly into the buffer that ``quirc`` uses for image processing:
-     * */
-    image = quirc_begin(q, NULL, NULL);
+    int imageArea = width * height;
+    uint8_t *buffer = malloc(sizeof(uint8_t) * imageArea);
 
     printf("number passes: %d\n", number_passes);
 
@@ -85,21 +88,21 @@ void load_png(struct quirc *q, const char *filename) {
         int y;
 
         for (y = 0; y < height; y++) {
-            png_bytep row_pointer = image + y * width;
+            png_bytep row_pointer = buffer + y * width;
             png_read_rows(png_ptr, &row_pointer, NULL, 1);
         }
     }
 
-    printf("width: %d\n", width);
-    printf("height: %d\n", height);
-    printf("rowbytes: %d\n", rowbytes);
-
-    printf("image ptr: %p\n", image);
+    img.width = width;
+    img.height = height;
+    img.buffer = buffer;
 
     png_read_end(png_ptr, info_ptr);
 
     if (infile)
         fclose(infile);
+
+    return img;
 }
 
 int decode_qr(char **argv) {
@@ -123,7 +126,22 @@ int decode_qr(char **argv) {
      * convert to grayscale image,
      * feed grayscale image ke buffer using quirc_end and quirc_begin.
      * */
-    load_png(q, argv[1]);
+    struct Image img = load_png(q, argv[1]);
+    printf("img width: %d\n", img.width);
+    printf("img height: %d\n", img.height);
+    printf("img buffer: %p\n", img.buffer);
+
+    uint8_t *p;
+    p = img.buffer;
+
+    unsigned int image_area = img.height * img.width;
+
+
+    /*check value and copy elements*/
+    for (int i = 0; i < image_area; ++i) {
+        printf("Value of image[%d] = %d\n", i, *p);
+        p++;
+    }
 
     /*
      * After filling the buffer, quirc_end() should be called to process
