f18f3459159d28a7c977684b92190c3662439d4c
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-16 17:07:10 +0700

    Update compile quirc wasm bash script

--- repos/webassembly-jpeg/compile.sh
+++ repos/webassembly-jpeg/compile.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-emcc -o webassembly-jpeg.js decode_jpeg.c quirc/*.c jpeg-read.c jpeg-write.c webassembly-jpeg.c libjpeg/.libs/libjpeg.a -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["writeArrayToMemory","getValue", "cwrap"]'
-#emcc  decode_jpeg.c quirc/*.c libjpeg/.libs/libjpeg.a -s WASM=1 -s NO_EXIT_RUNTIME=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["writeArrayToMemory","getValue", "cwrap"]'
-ls -l -h | grep wasm
+emcc -o quirc.js decode_jpeg.c quirc/*.c jpeg-read.c webassembly-jpeg.c libjpeg/.libs/libjpeg.a -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["writeArrayToMemory","getValue", "cwrap"]'
+ls -l -h | grep quirc.wasm
