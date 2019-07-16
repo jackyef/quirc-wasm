cba9482a236d1b4cf590fd212abc129f3c4e6d1f
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-16 17:44:07 +0700

    Delete unused bash script and default quirc source codes

--- compile-memdjpeg.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-printf "Make Clean\n";
-sudo make clean
-printf "Make memdpeg\n"
-sudo make memdjpeg
-printf "Execute memdjpeg\n"
-./memdjpeg ./qrcodes/tokopedia.jpg
-./memdjpeg ./qrcodes/pdp-tokopedia.jpg
--- test-qr-decoder.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-./qr-decoder ./qrcodes/tokopedia.png
-./qr-decoder ./qrcodes/pdp-tokopedia.png
