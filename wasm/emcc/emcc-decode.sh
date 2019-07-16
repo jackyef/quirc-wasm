5d6a7c286e30879be4806b45df5ced4bb0c8471e
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-16 11:09:46 +0700

    Compile decode_jpeg to wasm experiment#2

--- wasm/emcc/emcc-decode.sh
+++ wasm/emcc/emcc-decode.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-emcc -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s ERROR_ON_MISSING_LIBRARIES=0 -s ALLOW_MEMORY_GROWTH=1 -s ASSERTIONS=1 \
-    -I decode_jpeg.c \
-    lib/*.c
+emcc -O3 decode_jpeg.c -o decode_jpeg.js -s WASM=1 -I quirc/*.c libjpeg/*.c
