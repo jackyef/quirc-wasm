816fbb6aa69f90607754f552661944ee305dc349
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-16 17:32:46 +0700

    Update compile wasm bash script Remove old wasm objects and js glue codes from the emcc build results.

--- wasm/compile.sh
+++ wasm/compile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+rm ./quirc.js ./quirc.wasm
 emcc -o quirc.js decode_jpeg.c quirc/*.c jpeg-read.c webassembly-jpeg.c libjpeg/.libs/libjpeg.a -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["writeArrayToMemory","getValue", "cwrap"]'
 ls -l -h | grep quirc.wasm
