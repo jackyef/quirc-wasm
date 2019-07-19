d56cabe3e929c41ecb71e2ce06659f773cc9175c
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-19 15:14:47 +0700

    Add pre-js argument to add onRuntimeInit functions

--- wasm/compile.sh
+++ wasm/compile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rm ./quirc.js ./quirc.wasm
-emcc -o quirc.js decode_jpeg.c quirc/*.c jpeg-read.c webassembly-jpeg.c libjpeg/.libs/libjpeg.a -O3 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["writeArrayToMemory","getValue", "cwrap"]'
+source ~/Documents/emsdk/emsdk_env.sh
+emcc -o quirc.js decode_jpeg.c quirc/*.c jpeg-read.c webassembly-jpeg.c libjpeg/.libs/libjpeg.a -O3 -s --pre-js quirc-js.js -s MODULARIZE_INSTANCE=1 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=1 -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["writeArrayToMemory","getValue", "cwrap"]'
 ./compress.sh
 ./size.sh
